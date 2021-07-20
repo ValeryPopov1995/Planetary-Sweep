4c79118d63ac29d85ac9022c09317a65e205a2dd
ValeryPopov1995 <72905449+Valer-jan@users.noreply.github.com>
2021-07-20 21:19:05 +0300

    Visual
    Post Processing - bloom
    Flashlight with cookie

--- Assets/ValerJan/Scripts/Player/PlayerMovenment.cs
+++ Assets/ValerJan/Scripts/Player/PlayerMovenment.cs
@@ -1,8 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovenment : MonoBehaviour
 {
     [SerializeField] Transform _body;
